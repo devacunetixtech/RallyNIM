5efc198d553a613be7a6266c4ddeee244f44437e
Adebisi Precious <adebisiprecious780@gmail.com>
2026-07-29 19:19:53 +0100

    fix again

--- frontend/src/lib/nimiq.ts
+++ frontend/src/lib/nimiq.ts
@@ -31,7 +31,7 @@ function toHexString(byteArray: Uint8Array): string {
 function hexToBytes(hex: string): Uint8Array {
   const bytes = new Uint8Array(hex.length / 2);
   for (let i = 0; i < bytes.length; i++) {
-    bytes[i] = parseInt(hex.substring(i * 2, 2), 16);
+    bytes[i] = parseInt(hex.substring(i * 2, (i * 2) + 2), 16);
   }
   return bytes;
 }
